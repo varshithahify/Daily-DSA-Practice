698eeebeffc3a5bae2eda2b5f4e2a8dd31a127b5
varshithahify <varshithavn26@gmail.com>
2026-02-19 06:48:51 +0530

    deleted old file

---
+++ src/dsapractice/Day1_CompareNM.java
@@ -0,0 +1,18 @@
+package dsapractice;
+
+//P1-Decision making in java(Basic)
+public class CompareNM {
+	public static String compareNM(int n, int m) {
+	       
+	        if(n<m){
+	            return "lesser";
+	        }
+	        else if(n==m){
+	            return "equal";
+	        }
+	        else{
+	            return "greater";
+	        }
+	    }
+	}
+
