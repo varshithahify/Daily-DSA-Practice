c55349637d495cf76fddd92899aa98c121e205f3
varshithahify <varshithavn26@gmail.com>
2026-02-21 04:19:31 +0530

    Added time Complexity problem

---
+++ src/dsapractice/Day3_TimeComplexity.java
@@ -0,0 +1,22 @@
+package dsapractice;
+
+public class Day3_TimeComplexity {
+	 public static void TimeComplexity(int n) {
+	        switch(n){
+	            case 1:
+	                System.out.println("logn");
+	                break;
+	            case 2:
+	                System.out.println("nlogn");
+	                break;
+	            case 3:
+	                System.out.println("n^2");
+	                break;
+	            case 4:
+	                System.out.println("n^2");
+	                break;
+	            default:
+	                System.out.println("Invalid");
+	        }
+	    }
+}
