4e6cad5973491c3ade37bae18df8d545d8c20f65
varshithahify <varshithavn26@gmail.com>
2026-02-24 20:47:16 +0530

    added sort by frequency program

---
+++ src/dsapractice/SortByFrequency.java
@@ -0,0 +1,30 @@
+package dsapractice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class SortByFrequency {
+	 public ArrayList<Integer> sortByFreq(int arr[]) {
+	        // add your code here
+	        HashMap<Integer, Integer> map = new HashMap<>();
+	            for (int num : arr) {
+	            map.put(num, map.getOrDefault(num, 0) + 1);
+	        }
+
+	        ArrayList<Integer> list = new ArrayList<>();
+	        for (int num : arr) {
+	            list.add(num);
+	        }
+
+	        Collections.sort(list, (a, b) -> {
+	            if (!map.get(a).equals(map.get(b))) {
+	                return map.get(b) - map.get(a);
+	            }
+	                return a - b;
+	        });
+
+	        return list;
+	    }
+	}
+
