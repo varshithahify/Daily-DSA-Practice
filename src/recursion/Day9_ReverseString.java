820be9189c0875f0a87b6e18f513903f0f86fb06
varshithahify <varshithavn26@gmail.com>
2026-03-09 18:53:34 +0530

    added program to reverse a given string

---
+++ src/recursion/Day9_ReverseString.java
@@ -0,0 +1,14 @@
+package recursion;
+
+public class Day9_ReverseString {
+	static void reverse(String str) {
+		if(str.length()==0)
+			return;
+		reverse(str.substring(1));
+		System.out.print(str.charAt(0));
+	}
+	
+public static void main(String[] args) {
+	reverse("HELLO");
+}
+}
