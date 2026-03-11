53c2aa6aaf53d2605bdec9e6d59b10bd0d609335
varshithahify <varshithavn26@gmail.com>
2026-03-11 21:26:30 +0530

    added program to count zeros and number of steps performed and sorted array using recursion

---
+++ src/recursion/Day11_CheckSortedArray.java
@@ -0,0 +1,14 @@
+package recursion;
+
+public class Day11_CheckSortedArray {
+	static boolean sorted(int[] arr,int index) {
+		if(index==arr.length-1)
+			return true;
+		return arr[index]<=arr[index+1]&&sorted(arr,index+1);
+	}
+public static void main(String[] args) {
+	int[] arr= {1,2,3,4,5,7};
+	System.out.println(sorted(arr,0));
+	
+}
+}
