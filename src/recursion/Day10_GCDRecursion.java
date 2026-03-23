1a5febc83f80fd8f26871e480fe7106b6fe28626
varshithahify <varshithavn26@gmail.com>
2026-03-23 22:42:17 +0530

    Added Greatest common divisor program

---
+++ src/recursion/Day10_GCDRecursion.java
@@ -0,0 +1,14 @@
+package recursion;
+
+public class Day10_GCDRecursion {
+	static int gcd(int a, int b) {
+		if (b==0)
+			return a;
+				
+		return gcd(b, a%b);
+	}
+	public static void main(String[] args) {
+		System.out.println(gcd(48,18));
+	}
+	}
+
