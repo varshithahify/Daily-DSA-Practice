c49ffee13f468574b67306cd8c63f8acb8f3c39d
varshithahify <varshithavn26@gmail.com>
2026-04-11 09:01:06 +0530

    added merge sort program using recursion

---
+++ src/recursion/Day20_MergeSort.java
@@ -0,0 +1,48 @@
+package recursion;
+
+import java.util.Arrays;
+
+public class Day20_MergeSort {
+	static int[] mergesort(int[] arr) { 
+		if(arr.length==1) {
+		return arr;
+	}
+		int mid=arr.length/2;
+		int[] left=mergesort(Arrays.copyOfRange(arr,0,mid));
+		int[] right=mergesort(Arrays.copyOfRange(arr,mid,arr.length));
+	return merge(left,right);
+	}
+	static int[] merge(int[] first,int[] second) {
+		int[] mix=new int[first.length+second.length];
+		int i=0;
+		int j=0;
+		int k=0;
+		
+		while(i<first.length && j<second.length) {
+			if(first[i]<second[j]) {
+				mix[k]=first[i];
+				i++;
+			}
+			else {
+				mix[k]=second[j];
+				j++;
+			}
+			k++;
+		}
+		while(i<first.length) {
+			mix[k]=first[i];
+			i++;
+			k++;
+		}
+		while(j<second.length) {
+			mix[k]=second[j];
+			j++;
+			k++;
+		}
+		return mix;
+	}
+public static void main(String[] args) {
+	int[] arr= {8,9,7,5,4};
+	arr=mergesort(arr);
+	System.out.println(Arrays.toString(arr));
+}}
