ccc50a1c1ac66a9a2ee68df1448a60b6f47aedd3
varshithahify <varshithavn26@gmail.com>
2026-04-22 07:43:08 +0530

    solved printing paths in all directions

---
+++ src/recursion/Day23_AllPaths.java
@@ -0,0 +1,39 @@
+package recursion;
+
+public class Day23_AllPaths {
+
+	static void allpath(String p,boolean[][] maze,int r, int c) {
+		if(!maze[r][c]) {
+			return;
+		}
+		if(r==maze.length-1&&c==maze[0].length-1) {
+			System.out.println(p);
+			return;
+		}
+		maze[r][c]=false;
+		if(r<maze.length-1) {
+			allpath(p+"D",maze,r+1,c);
+		}
+	
+	if(c<maze[0].length-1) {
+		allpath(p+"R",maze,r,c+1);
+	}
+	if(r>0) {
+		allpath(p+"U",maze,r-1,c);
+	}
+	if(c>0) {
+		allpath(p+"L",maze,r,c-1);
+	}
+	maze[r][c]=true;
+	}
+public static void main(String[] args) {
+	boolean[][] maze= {
+			{true,true,true},
+			{true,true,true},
+			{true,true,true}
+	};
+	allpath("",maze,0,0);
+}
+
+}
+
